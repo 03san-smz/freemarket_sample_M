4dee1c73ff304dbe30132565e6f35e4459a87c39
haccaman <haccaman@gmail.com>
2019-04-08 14:24:08 +0900

    [WIP]【マークアップ】ユーザーマイページ
    アイテム詳細ページ

--- app/assets/javascripts/bxslider.js
+++ app/assets/javascripts/bxslider.js
@@ -1,6 +1,27 @@
+// item/mainカルーセル
 $(function(){
   $('.bxslider').bxSlider({
     auto: true,
     adaptiveHeight: true
+  });
+});
+
+
+//item/item-detailカルーセル
+$(function(){
+  $('.photo-slider').bxSlider({
+    pagerCustom: '.photo-thumbnail',
+    controls:false
+  });
+});
+
+//item/item-detailサムネイル
+$(function(){
+  $('.photo-thumbnail').bxSlider({
+    pager:false,
+    minSlides: 4,
+    maxSlides: 4,
+    slideWidth: 75,
+    slideMargin: 0,
   });
 });
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :signup, only: [:new]
   resources :transactions, only: [:new, :edit]
   resources :sales, only: [:new]
+  resources :items, only: [:show]
+
   get 'signup/login' => 'signup#login'
   get 'mypage/identification' => 'mypage#edit_identification'
   get 'mypage', to: 'mypages#index'
